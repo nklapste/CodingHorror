0cf5d3e37e2032943875507dd91712a705c0e96c
nklapste <nklapste@ualberta.ca>
2017-10-06 09:01:23 -0600

    adding coding horror source

--- src/CodingHorror.java
+++ src/CodingHorror.java
@@ -1,9 +1,29 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
+/**
+ * Assignment 4 Part 1: Static Code Analysis <br />
+ * The buggy {@code CodingHorror} source code
+ */
 public class CodingHorror {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String args[]) {
+        // TODO: Assignment 4 Part 1 -- run FindBugs on the code
 
-	}
-
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String input = null;
+        try {
+            input = br.readLine();                  // e.g., peel
+        } catch (IOException ioex) {
+            System.err.println(ioex.getMessage());
+        }
+        input.replace('e', 'o');
+        if (input == "pool") {
+            System.out.println("User entered peel.");
+        } else {
+            System.out.println("User entered something else.");
+        }
+    }
 }
